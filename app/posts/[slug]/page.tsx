5541db09a8f0b97481a9d4be08c938aa71bd3934
Sergei Usachev <usachev97@gmail.com>
2025-03-15 00:46:52 +0100

    фикс типа параметров (спасибо доке)

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -10,9 +10,10 @@ export async function generateStaticParams() {
 export default async function PostPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
-  const post = await getPostBySlug(params.slug);
+  const { slug } = await params;
+  const post = getPostBySlug(slug);
 
   if (!post) return notFound();
 
